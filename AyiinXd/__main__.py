a78093ecd74231a9fccc3345b5d4160ea03333f8
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-11 01:55:45 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -40,7 +40,7 @@ try:
     LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
     LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
     LOGS.info(f"Userbot Version - {ubotversion} •[{adB.name}]•")
-    LOGS.info(f"Ayiin Version - {ayiin_version} •[{HOSTED_ON}]•")
+    LOGS.info(f"Nath Version - {ayiin_version} •[{HOSTED_ON}]•")
     LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
