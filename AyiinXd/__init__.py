18aca5d947abb0a56a90d9c31a0aad589699e873
nathaell <110729662+nathxe@users.noreply.github.com>
2022-10-07 19:26:16 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -216,13 +216,13 @@ ALIVE_TEKS_CUSTOM = os.environ.get(
 ALIVE_NAME = os.environ.get("ALIVE_NAME", "im_el69")
 
 # Custom Emoji Alive
-ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "✩")
+ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "⋆")
 
 # Custom Emoji Alive
-INLINE_EMOJI = os.environ.get("INLINE_EMOJI", "✩")
+INLINE_EMOJI = os.environ.get("INLINE_EMOJI", "⋆")
 
 # Custom icon HELP
-ICON_HELP = os.environ.get("ICON_HELP", "✩")
+ICON_HELP = os.environ.get("ICON_HELP", "⋆")
 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", "ID"))
