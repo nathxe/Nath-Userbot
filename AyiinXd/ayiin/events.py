00d7e8f2a15e0174613b30aee5ab6b81a47b5ab2
nathaell <110729662+nathxe@users.noreply.github.com>
2022-10-11 02:31:18 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -79,8 +79,8 @@ async def get_user_from_event(
 
 
 async def checking(client):
-    gocheck = str(pybase64.b64decode("QG5hdGhhZWxseHg="))[2:15]
-    checker = str(pybase64.b64decode("QG5hdGhzdXBwb3J0"))[2:17]
+    gocheck = str(pybase64.b64decode("QG5hdGhhZWxseHg="))[2:13]
+    checker = str(pybase64.b64decode("QG5hdGhzdXBwb3J0"))[2:15]
     checkxd = str(pybase64.b64decode("QHRlcnVzYXBhbGFnaXlh"))[2:16]
     if client:
         try:
